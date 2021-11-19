eee0a029c201761f083f4b6e21c4587eeab96819
Ahmed <ahmedrather.s@gmail.com>
2021-11-18 22:20:54 -0500

    basic login done

--- quicktracker-app/src/components/App.js
+++ quicktracker-app/src/components/App.js
@@ -1,8 +1,20 @@
+import Auth0ProviderWithHistory from "../auth0Provider";
+import { Route, Switch } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Header from "./Header";
+
+require("dotenv").config();
+
 function App() {
+  console.log("domain " + process.env.REACT_APP_AUTH0_DOMAIN);
+  console.log("id " + process.env.REACT_APP_AUTH0_CLIENT_ID);
+
   return (
-    <div className="App">
-      <header className="App-header">hello world</header>
-    </div>
+    <Auth0ProviderWithHistory>
+      <div className="App">
+        <Header />
+      </div>
+    </Auth0ProviderWithHistory>
   );
 }
 
---
+++ quicktracker-app/src/components/Header.js
@@ -0,0 +1,56 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+import React from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+function Header() {
+  const { loginWithRedirect, logout, user, isLoading } = useAuth0();
+  return (
+    <Container fluid>
+      <Navbar bg="light" expand="sm" sticky="top">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>QuickTracker</Navbar.Brand>
+          </LinkContainer>{" "}
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              {/* idk if this home is needed*/}
+              <LinkContainer to="postmedia">
+                <Nav.Link>Post Media</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="books">
+                <Nav.Link>Books</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="movies">
+                <Nav.Link>Movies</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="songs">
+                <Nav.Link>Songs</Nav.Link>
+              </LinkContainer>
+              {!isLoading && !user && (
+                <button
+                  className="btn btn-primary btn-block"
+                  onClick={() => loginWithRedirect()}
+                >
+                  Log In
+                </button>
+              )}
+              {!isLoading && user && (
+                <button
+                  className="btn btn-primary btn-block"
+                  onClick={() => logout()}
+                >
+                  Log Out
+                </button>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Container>
+  );
+}
+
+export default Header;
