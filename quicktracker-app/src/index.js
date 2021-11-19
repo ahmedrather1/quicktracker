c9425ec519a0990ccb792c3645b20f8bd9c95551
Ahmed <ahmedrather.s@gmail.com>
2021-11-18 22:41:11 -0500

    redux setup

--- quicktracker-app/src/index.js
+++ quicktracker-app/src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import store from "./redux/store";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ quicktracker-app/src/redux/LoginSlice.js
@@ -0,0 +1,28 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const LoginSlice = createSlice({
+  name: "login",
+  initialState: { login: { isLoggedIn: false, email: null, name: null } },
+  reducers: {
+    logIn: (state, action) => {
+      return {
+        login: {
+          isLoggedIn: true,
+          email: action.payload.email,
+          name: action.payload.name,
+        },
+      };
+    },
+    logOut: (state, action) => {
+      console.log("logged out fro slice");
+      return { login: { isLoggedIn: false, email: null, name: null } };
+    },
+  },
+  extraReducers: {},
+});
+
+export const { logIn, logOut } = LoginSlice.actions;
+
+export const loginSelector = (state) => state.login;
+
+export default LoginSlice.reducer;
